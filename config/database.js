c7cf36cbd816fc0bd2c08024478de2040389b3d6
Jada-Cherie <jada.c.townsend@gmail.com>
2023-05-01 09:17:14 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://showmovie:showmovie@showmovie.hmfvq5b.mongodb.net/showmovie?retryWrites=true&w=majority', 
+    'dbName': 'showmovie'
+};
